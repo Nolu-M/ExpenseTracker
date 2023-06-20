d48288710a8def5f2ae6cbfc3b629f323e6a8ef8
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-06-20 22:08:52 +0200

    Added Sidebar

---
+++ src/components/SidebarData.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as GrIcons from 'react-icons/gr';
+
+export const SidebarData = [
+    {
+        title: "Home",
+        path: '/',
+        icons: <AiIcons.AiFilHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: "Services",
+        path: '/services',
+        icons: <GrIcons.GrServices />,
+        cName: 'nav-text',
+    },
+    {
+        title: "Reports",
+        path: '/reports',
+        icons: <IoIcons.IoIosPaper />,
+        cName: 'nav-text',
+    }
+]
+
+
+export default SidebarData
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Reports from './pages/Reports';
+import Navbar from './components/Navbar'
+import "./index.css";
 
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/reports",
+        element: <Reports />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
